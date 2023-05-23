47bb6560a5a11abdd579be47ff7f99a41f38afdc
Elaine <elainedoud@gmail.com>
2023-05-23 15:40:27 -0400

    Added thicker border, drop shadow, and gray topic names

--- client/src/Accepted.js
+++ client/src/Accepted.js
@@ -3,15 +3,15 @@ import React from 'react';
 function Accepted({acceptedlist}){
     return (
         <div class="col-md-6 p-3">
-        <div class="card border-secondary">
-            <div class="card-block">
+        <div class="card border-secondary border-3">
+            <div class="card-block shadow-lg">
                 <div class="card-body bg-light">
-                    <h6 class="card-title text-start text-dark">Name: {acceptedlist.apartment_name}</h6>
-                    <h6 class="card-text text-start text-dark">Address: {acceptedlist.address}</h6>
-                    <h6 class="card-text text-start text-dark">Price: ${acceptedlist.price}</h6>
-                    <h6 class="card-text text-start text-dark">Contact: {acceptedlist.contact_info}</h6>
-                    <h6 class="card-text text-start text-dark">Website: {acceptedlist.website}</h6>
-                    <h6 class="card-text text-start text-dark">Comment: {acceptedlist.comment}</h6>
+                    <h6 class="card-title text-start text-dark"><span class="Light-text">Name: </span>{acceptedlist.apartment_name}</h6>
+                    <h6 class="card-text text-start text-dark"><span class="Light-text">Address: </span>{acceptedlist.address}</h6>
+                    <h6 class="card-text text-start text-dark"><span class="Light-text">Price: </span>${acceptedlist.price}</h6>
+                    <h6 class="card-text text-start text-dark"><span class="Light-text">Contact: </span>{acceptedlist.contact_info}</h6>
+                    <h6 class="card-text text-start text-dark"><span class="Light-text">Website: </span>{acceptedlist.website}</h6>
+                    <h6 class="card-text text-start text-dark"><span class="Light-text">Comment: </span>{acceptedlist.comment}</h6>
                 </div>
             </div>
         </div>
--- client/src/NotSelected.js
+++ client/src/NotSelected.js
@@ -3,15 +3,15 @@ import React from 'react';
 function NotSelected({notselectedlist}){
     return (
         <div class="col-md-6 p-3">
-            <div class="card border-secondary">
-                <div class="card-block">
+            <div class="card border-secondary border-3">
+                <div class="card-block shadow-lg">
                     <div class="card-body bg-light">
-                        <h6 class="card-title text-start text-dark">Name: {notselectedlist.apartment_name}</h6>
-                        <h6 class="card-text text-start text-dark">Address: {notselectedlist.address}</h6>
-                        <h6 class="card-text text-start text-dark">Price: ${notselectedlist.price}</h6>
-                        <h6 class="card-text text-start text-dark">Contact: {notselectedlist.contact_info}</h6>
-                        <h6 class="card-text text-start text-dark">Website: {notselectedlist.website}</h6>
-                        <h6 class="card-text text-start text-dark">Comment: {notselectedlist.comment}</h6>
+                        <h6 class="card-title text-start text-dark"><span class="Light-text">Name: </span>{notselectedlist.apartment_name}</h6>
+                        <h6 class="card-text text-start text-dark"><span class="Light-text">Address: </span>{notselectedlist.address}</h6>
+                        <h6 class="card-text text-start text-dark"><span class="Light-text">Price:</span> ${notselectedlist.price}</h6>
+                        <h6 class="card-text text-start text-dark"><span class="Light-text">Contact: </span>{notselectedlist.contact_info}</h6>
+                        <h6 class="card-text text-start text-dark"><span class="Light-text">Website: </span>{notselectedlist.website}</h6>
+                        <h6 class="card-text text-start text-dark"><span class="Light-text">Comment: </span>{notselectedlist.comment}</h6>
                     </div>
                 </div>
             </div>
